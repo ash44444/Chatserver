5a30209dd71b87ce12a7acb3e31a597d5aa4a6d0
AshishManer <108808771+ash44444@users.noreply.github.com>
2024-07-17 06:45:41 +0530

    Update config.js
    Fggh

--- constants/config.js
+++ constants/config.js
@@ -2,7 +2,8 @@ const corsOptions = {
   origin: [
     "http://localhost:5173",
     "http://localhost:4173",
-    process.env.CLIENT_URL,
+  
+  "chatfrontend-one.vercel.app"
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
